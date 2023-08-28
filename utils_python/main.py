d274da8da5d9d28d077b0184f36a1ca6ab69a1b3
qwrwed <qwrwed@users.noreply.github.com>
2023-08-28 15:06:14 +0100

    stringify keys in dump_data

--- utils_python/main.py
+++ utils_python/main.py
@@ -1,4 +1,5 @@
 import json
+from copy import deepcopy
 
 
 def flatten(l: list[list]):
@@ -14,6 +15,41 @@ def deduplicate(l: list):
 
 
 def dump_data(data: any, filepath="tmp.json"):
-    data_str = json.dumps(data, indent=4)
+    if isinstance(data, str):
+        data_str = data
+    else:
+        indent = 4
+        try:
+            data_str = json.dumps(data, indent=indent)
+        except TypeError:
+            data_str = json.dumps(stringify_keys(data), indent=indent)
     with open(filepath, "w+") as f:
         f.write(data_str)
+
+
+def stringify_keys(d: dict):
+    """
+    Convert a dict's keys to strings if they are not.
+    https://stackoverflow.com/a/51051641
+    """
+    d_copy = deepcopy(d)
+    for key in d.keys():
+        # check inner dict
+        if isinstance(d[key], dict):
+            value = stringify_keys(d[key])
+        else:
+            value = d[key]
+
+        # convert nonstring to string if needed
+        if not isinstance(key, str):
+            try:
+                d_copy[str(key)] = value
+            except Exception:
+                try:
+                    d_copy[repr(key)] = value
+                except Exception:
+                    raise
+
+            # delete old key
+            del d_copy[key]
+    return d_copy
