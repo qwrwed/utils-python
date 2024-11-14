1fe922e1b5677d5e4da98bb7a26b9019a7a7df1f
qwrwed <2429893+qwrwed@users.noreply.github.com>
2024-11-14 03:02:14 +0000

    feature: add 'set_custom_tag_mp4_text()'

--- utils_python/__init__.py
+++ utils_python/__init__.py
@@ -2,6 +2,7 @@ from .utils_data import *
 from .utils_files import *
 from .utils_logging import *
 from .utils_main import *
+from .utils_media import *
 from .utils_strings import *
 from .utils_tqdm import *
 from .utils_typing import *
---
+++ utils_python/utils_media.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+
+from mutagen.easymp4 import EasyMP4
+from mutagen.mp4 import MP4FreeForm
+
+
+def set_custom_tag_mp4_text(
+    filepath: Path,
+    key: str,
+    value: str,
+    *,
+    atomid: str | None = None,
+):
+    if atomid is None:
+        atomid = f"----:com.apple.iTunes:{key.upper()}"
+    EasyMP4.RegisterTextKey(key, atomid)
+
+    value_encoded = MP4FreeForm(value.encode())
+    mp4 = EasyMP4(filepath)
+    mp4[key] = value_encoded
+    mp4.save()
