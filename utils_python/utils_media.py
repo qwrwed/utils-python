79b23c703e0852f50a37b33d425847c3cc88f6fe
qwrwed <2429893+qwrwed@users.noreply.github.com>
2024-11-19 10:42:53 +0000

    fix: separate mp4 tag registering from setting (so that it could be used for getting as well)

--- utils_python/utils_media.py
+++ utils_python/utils_media.py
@@ -4,6 +4,15 @@ from mutagen.easymp4 import EasyMP4
 from mutagen.mp4 import MP4FreeForm
 
 
+def register_custom_tag_mp4_text(
+    key: str,
+    atomid: str | None = None,
+):
+    if atomid is None:
+        atomid = f"----:com.apple.iTunes:{key.upper()}"
+    EasyMP4.RegisterTextKey(key, atomid)
+
+
 def set_custom_tag_mp4_text(
     filepath: Path,
     key: str,
@@ -11,10 +20,7 @@ def set_custom_tag_mp4_text(
     *,
     atomid: str | None = None,
 ):
-    if atomid is None:
-        atomid = f"----:com.apple.iTunes:{key.upper()}"
-    EasyMP4.RegisterTextKey(key, atomid)
-
+    register_custom_tag_mp4_text(key, atomid)
     value_encoded = MP4FreeForm(value.encode())
     mp4 = EasyMP4(filepath)
     mp4[key] = value_encoded
